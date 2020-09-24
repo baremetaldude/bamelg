634c5fae406f9e46967879b373c9916fa5c192ea
Eugeny Grishul <eugeny.grishul@gmail.com>
2020-09-24 21:02:05 +0300

    refactoring

--- examples/Linux/Fedora 29/BuildScripts/msbuild32.distribution.x64.cmd
+++ examples/Linux/Fedora 29/BuildScripts/msbuild32.distribution.x64.cmd
@@ -4,5 +4,5 @@
 
 pushd "%~dp0\.."
 call ..\..\..\vs_environment.cmd
-MSBuild.exe /nologo /t:Build /property:Configuration=Distribution;TargetCpu=X86_64 /v:d App.bmproj
+MSBuild.exe /nologo /t:Build /property:Configuration=Distribution;TargetCpu=X86_64;IntermediateOutput=true /v:d App.bmproj
 popd
